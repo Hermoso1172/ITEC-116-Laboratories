d64f343710f49e0feccca7af626506bee4552140
Hermoso1172 <ic.clarenceherold.hermoso@cvsu.edu.ph>
2025-10-13 11:25:00 +0800

    calendar
    pero di pa tapos di ko ma palaki ung calendar

--- Activity 1/client/src/pages/Calendar.jsx
+++ Activity 1/client/src/pages/Calendar.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import App from "../App"
 
 function Calendar() {
-  return <div>Calendar</div>;
+  return (
+    <div className="w-full h-full p-4 bg-white rounded-lg shadow">
+      <h1 className="text-xl font-semibold mb-4">Calendar</h1>
+      <div className="w-full h-full">
+        <DatePicker inline calendarClassName="custom-calendar" />
+      </div>
+    </div>
+  );
 }
 
 export default Calendar;
