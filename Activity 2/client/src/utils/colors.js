1209a7ba44530df6712b83e312adc7c6e78c9019
MGChristian <132035945+MGChristian@users.noreply.github.com>
2025-10-25 23:38:52 +0800

    Update colors.js

--- Activity 2/client/src/utils/colors.js
+++ Activity 2/client/src/utils/colors.js
@@ -1,40 +1,19 @@
 const folderColors = [
-  {
-    name: "RED",
-    value: "#FF4848",
-  },
-  {
-    name: "ORANGE",
-    value: "#E69819",
-  },
-  {
-    name: "YELLOW",
-    value: "#F3EC1B",
-  },
-  {
-    name: "GREEN",
-    value: "#26E933",
-  },
-  {
-    name: "BLUE",
-    value: "#2DC1E3",
-  },
-  {
-    name: "CYAN",
-    value: "#2D41C5",
-  },
-  {
-    name: "PINK",
-    value: "#DE27C6",
-  },
+  { name: "RED", value: "#FF7F7F" },
+  { name: "ORANGE", value: "#FFCC99" },
+  { name: "YELLOW", value: "#FFF3B0" },
+  { name: "GREEN", value: "#B0FFB3" },
+  { name: "BLUE", value: "#99E6FF" },
+  { name: "CYAN", value: "#B3CCFF" },
+  { name: "PINK", value: "#FFB3E6" },
 ];
 
 export const colors = {
-  RED: "#FF4848",
-  ORANGE: "#E69819",
-  YELLOW: "#F3EC1B",
-  GREEN: "#26E933",
-  BLUE: "#2DC1E3",
-  CYAN: "#2D41C5",
-  PINK: "#DE27C6",
+  RED: "#FF7F7F",
+  ORANGE: "#FFCC99",
+  YELLOW: "#FFF3B0",
+  GREEN: "#B0FFB3",
+  BLUE: "#99E6FF",
+  CYAN: "#B3CCFF",
+  PINK: "#FFB3E6",
 };
